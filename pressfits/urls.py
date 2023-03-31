c10f00ce17479b51639a9b5430b32b1e635eadd7
Samuel Lehmann <samueljlehmann@gmail.com>
2023-03-31 07:42:12 -0700

    Rest framework setup

--- pressfits/urls.py
+++ pressfits/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
+from django.contrib.auth.models import User
+from django.urls import include, path
+from rest_framework import routers
 
 from . import views
 
-urlpatterns = [path("", views.PressFit.as_view())]
+router = routers.DefaultRouter()
+router.register(r"PressFitData", views.PressView, basename="PressFitData")
+
+urlpatterns = [
+    path("", views.PressFit.as_view()),
+    path("press", views.PressView.as_view()),
+]
--- pressfits/views.py
+++ pressfits/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.views import View
+from rest_framework import serializers
+from rest_framework import views as REST_Views
+from rest_framework.response import Response
+
+
+class PressSerializer(serializers.Serializer):
+
+    mesh_string = serializers.CharField()
+    elemental_results_string = serializers.CharField()
 
 
 class PressFit(View):
     def get(self, request):
         return render(request, "pressfits/page.html")
+
+
+class PressView(REST_Views.APIView):
+    def get(self, request):
+        sample_data = {
+            "mesh_string": "Sample Mesh",
+            "elemental_results_string": "Sample Elemental Results",
+        }
+
+        results = PressSerializer(sample_data).data
+        return Response(results)
